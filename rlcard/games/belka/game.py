004083c5f0d45a5bd6f8321562c8ee0a7baf29e0
macuser <macuser@mbar.local>
2020-06-01 17:54:55 +0600

    small additions

--- rlcard/games/belka/game.py
+++ rlcard/games/belka/game.py
@@ -27,10 +27,40 @@ class BelkaGame(object):
 
         return state, player_id
 
+    def step(self, action):
+        if self.allow_step_back:
+            his_dealer = deepcopy(self.dealer)
+            his_round = deepcopy(self.round)
+            his_players = deepcopy(self.players)
+            self.history.append((his_dealer, his_players, his_round))
+
+        self.round.proceed_round(self.players, action)
+        player_id = self.round.current_player
+        state = self.get_state(player_id)
+        return state, player_id
+
     def get_legal_actions(self):
         return self.round.get_legal_actions(self.players, self.round.current_player, self.trump_suit)
 
 
     def get_trump_suit(self, players):
+        
 
-
+if __name__ == '__main__':
+    #import time
+    #random.seed(0)
+    #start = time.time()
+    game = BelkaGame()
+    for _ in range(1):
+        state, button = game.init_game()
+        print(button, state)
+        for _ in range(8):
+            legal_actions = game.get_legal_actions()
+            print('legal_actions', legal_actions)
+            action = np.random.choice(legal_actions)
+            print('action', action)
+            print()
+            state, button = game.step(action)
+            print(button, state)
+        print(game.get_payoffs())
+    print('step', i)
--- rlcard/games/belka/round.py
+++ rlcard/games/belka/round.py
@@ -4,6 +4,11 @@ class BelkaRound(object):
     def __init__(self, dealer, num_players, np_random):
         self.np_random = np_random
         self.dealer = dealer
+        self.first_card = None
+        self.current_player = 0
+        self.num_players = 4
+        self.played_cards = []
+        self.winner = None
 
     def get_legal_actions(self, players, player_id, trump_suit):
         legal_actions = []
@@ -31,4 +36,23 @@ class BelkaRound(object):
 
         return legal_actions
 
+    def proceed_round(self, players, action):
+        player = players[self.current_player]
+        card_info = action.split('-')
+        rank = card_info[0]
+        suit = card_info[1]
+
+        remove_index = None
+        for index, card in enumerate(player.hand):
+            if rank == card.rank and suit == card.suit:
+                remove_index = index
+
+        card = player.hand.pop(remove_index)
+        self.played_cards.append(card)
+
+    def get_state(self, players, player_id):
+        state = {}
+        player = players[player_id]
+        state['hand'] = cards2list(player.hand)
+        state['target'] = self.target.str
     
